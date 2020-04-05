bd7aebabee0cefdd8c2470018ac5ee0ae1f1a499
Yang Shi <yangshi0302@gmail.com>
2020-04-05 14:27:17 -0400

    finished sydrome calc and correpution checking .

--- stdrscoding.h
+++ stdrscoding.h
@@ -2,5 +2,7 @@
 #include "arithmetic.h"
 
 struct Polynomial *rs_encode_msg(struct Polynomial *, int, struct Tables *);
+
 struct Polynomial *rs_calc_syndromes(struct Polynomial *, int, struct Tables *);
+
 int rs_check(struct Polynomial *, int, struct Tables *);
