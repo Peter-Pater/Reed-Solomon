abb441b0d4a3b803db0c4c0c7c19ada34f6d2245
Xincheng Huang <xh721@nyu.edu>
2020-04-29 19:06:51 -0400

    sample

--- comprs/comp_arithmetic.c
+++ comprs/comp_arithmetic.c
@@ -12,15 +12,8 @@ long gf_mul_comp(long x, long y, struct Tables *tables){
     long b0 = y & 0xff; //11001101
     // printf("lower order byte of y: %ld\n", a0);
 
-    // long p_arr[3] = {1, 1, 32};
-    // struct Polynomial *p = newPolynomial(p_arr, 3);
-    //
-    // long ret_arr[3] = {gf_mul_MR_BCH_8bits_LUT(a1, b1, tables), gf_mul_MR_BCH_8bits_LUT(a1, b0, tables) ^ gf_mul_MR_BCH_8bits_LUT(a0, b1, tables), gf_mul_MR_BCH_8bits_LUT(a0, b0, tables)};
-    // struct Polynomial *ret = newPolynomial(ret_arr, 3);
-
     long r1 = gf_mul_MR_BCH_8bits_LUT(a1, b0, tables) ^ gf_mul_MR_BCH_8bits_LUT(a0, b1, tables) ^ gf_mul_MR_BCH_8bits_LUT(a1, b1, tables);
     long r0 = gf_mul_MR_BCH_8bits_LUT(a0, b0, tables) ^ gf_mul_MR_BCH_8bits_LUT(gf_mul_MR_BCH_8bits_LUT(32, a1, tables), b1, tables);
-
 
     // printf("Higher order byte of result is: %ld\n", r1);
     // printf("Lower order byte of result is: %ld\n", r0); // 44799
--- comprs/main.c
+++ comprs/main.c
@@ -2,6 +2,57 @@
 #include <time.h>
 #include "comp_rscoding.h"
 
+void sample(){
+    int bits = 8;
+    int prime_polynomial = 285; //100011101
+    // int prime_polynomial_16 = 66525; //10000001111011101
+    // long prime_polynomial_32 = 4299161607; //100000000010000000000000000000111
+
+    struct Tables *tables = newTables(prime_polynomial, bits);
+
+    int n = 12000;
+    int k = 10000;
+
+    long message_arr[k];
+
+    for (int i = 0; i < k; i++)
+    {
+        *(message_arr + i) = i;
+    }
+
+    struct Polynomial *mesecc_poly = newPolynomial(message_arr, k);
+
+    struct Polynomial *encoded_mesecc_poly = rs_encode_msg(mesecc_poly, n - k, tables);
+    printf("Original Encoded Message:\n");
+    printPolynomial(encoded_mesecc_poly);
+    printf("\n");
+
+    // Tampering num_errors characters of the message:
+    int num_errors = 1000;
+    for (int i = 0; i < num_errors; i++){
+        *(encoded_mesecc_poly->poly_arr + i * 10) = 6666;
+    }
+
+    printf("Corrupted Encoded Message:\n");
+    printPolynomial(encoded_mesecc_poly);
+    printf("\n");
+
+
+    struct Polynomial *corrected_message_poly = rs_correct_msg(encoded_mesecc_poly, n - k, tables, 16);
+    printf("Repaired Message:\n");
+    printPolynomial(corrected_message_poly);
+
+    delTables(tables);
+    delPolynomial(encoded_mesecc_poly);
+}
+
+int main()
+{
+    sample();
+    return 0;
+}
+
+// obsolete tests
 void comprs_test(){
     // int prime_polynomial_16 = 102231; //10000001111011101
     // 66525, 69643, 69765, 79555, 80075, 80967, 83211, 94317, 95361, 99439, 101303, 101615, 101959, 102231
@@ -54,168 +105,4 @@ void comprs_test(){
     // long *t = malloc(sizeof(long));
     // ext_euclid(a, b, d, s, t, tables);
     // printf("%ld, %ld, %ld\n", *d, *s, *t); // d = gcd(a, b), as + bt = d
-}
-
-void stdrs_test(){
-    int bits = 8;
-    int prime_polynomial = 285; //100011101
-    int prime_polynomial_16 = 66525; //10000001111011101
-    long prime_polynomial_32 = 4299161607; //100000000010000000000000000000111
-
-    clock_t start, end;
-    double time_taken;
-
-    start = clock();
-    struct Tables *tables = newTables(prime_polynomial, bits);
-    end = clock();
-    time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("time taken for precomputing the table is %lf\n", time_taken);
-
-    //Example1
-    printf("Example1:\n");
-    int n = 500;
-    int k = 480;
-    // int message[300] = {1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                 1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000,
-    //                   1000, 1000, 2000, 2000, 3000, 1000, 1000, 2000, 2000, 3000};
-    char message[480] = {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                         'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n',
-                          'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '\n'
-                          };
-
-    // char message[9600];
-    // for (int i = 0; i < 9600; i++){
-    //     message[i] = 'h';
-    // }
-
-    long message_arr[k];
-    for (int i = 0; i < k; i++)
-    {
-        *(message_arr + i) = *(message + i);
-    }
-
-    struct Polynomial *mesecc_poly = newPolynomial(message_arr, k);
-
-    // time encoding
-    // start = clock();
-    // for (int i = 0; i < 10000000 / k; i++){
-    //     struct Polynomial *temp;
-    //     temp = rs_encode_msg(mesecc_poly, n - k, tables);
-    //     delPolynomial(temp);
-    // }
-    // end = clock();
-    // time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;
-    // printf("time taken for encoding %lf\n", time_taken);
-
-    struct Polynomial *encoded_mesecc_poly = rs_encode_msg(mesecc_poly, n - k, tables);
-    printf("Original Encoded Message:\n");
-    printPolynomial(encoded_mesecc_poly);
-    printf("\n");
-
-    // Tampering num_errors characters of the message:
-    int num_errors = 10;
-    *(encoded_mesecc_poly->poly_arr + 10) = 4000;
-    *(encoded_mesecc_poly->poly_arr + 20) = 4000;
-    *(encoded_mesecc_poly->poly_arr + 30) = 4000;
-    *(encoded_mesecc_poly->poly_arr + 40) = 4000;
-    *(encoded_mesecc_poly->poly_arr + 50) = 4000;
-    *(encoded_mesecc_poly->poly_arr + 60) = 4000;
-    *(encoded_mesecc_poly->poly_arr + 70) = 4000;
-    *(encoded_mesecc_poly->poly_arr + 80) = 4000;
-    *(encoded_mesecc_poly->poly_arr + 90) = 4000;
-    *(encoded_mesecc_poly->poly_arr + 100) = 4000;
-
-    printf("Corrupted Encoded Message:\n");
-    printPolynomial(encoded_mesecc_poly);
-    printf("\n");
-
-    // time decoding
-    // start = clock();
-    // for (int i = 0; i < 10000000 / k; i++){
-    //     struct Polynomial *temp;
-    //     temp = rs_correct_msg(encoded_mesecc_poly, n - k, tables, bits);
-    //     delPolynomial(temp);
-    // }
-    // end = clock();
-    // time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;
-    // printf("time taken for decoding %lf\n", time_taken);
-
-    struct Polynomial *corrected_message_poly = rs_correct_msg(encoded_mesecc_poly, n - k, tables, 16);
-    printf("Repaired Message:\n");
-    printPolynomial(corrected_message_poly);
-    // printPolynomialAsMessage(corrected_message_poly, k);
-
-    delTables(tables);
-    delPolynomial(encoded_mesecc_poly);
-}
-
-int main()
-{
-    stdrs_test();
-    // comprs_test();
-    return 0;
 }
